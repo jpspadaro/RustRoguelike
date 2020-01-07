77575a429268f8af180ef12f5b41542d950cd6d6
admin <admin@mac5061.local>
2020-01-07 13:45:27 -0500

    Working mapping in part 2 of tutorial.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,75 @@ use tcod::console::*;
 //Actual size of the window
 
 const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
+const SCREEN_HEIGHT: i32 = 45;
 
 const LIMIT_FPS: i32 = 20; //20 FPS max
 
+const COLOR_DARK_WALL: Color = Color {r: 0, g: 0, b: 100};
+const COLOR_DARK_GROUND: Color = Color {r: 0, g: 50, b: 150,};
+
+///A tile of the map and its properties
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+	    blocked: false,
+	    block_sight: false,
+	}
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: false,
+	    block_sight: false,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Tcod {
     root: Root,
     con: Offscreen,
 }
 
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+fn make_map() -> Map {
+    // fill map with unblocked tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    //Two test pillars for map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]){
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+	    let wall = game.map[x as usize][y as usize].block_sight;
+	    if wall {
+	        tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+	    } else {
+	        tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+	}
+    }
+    blit( &tcod.con, (0,0), (MAP_WIDTH, MAP_HEIGHT), &mut tcod.root, (0,0), 1.0, 1.0,);
+}
 
 struct Object {
     x: i32,
@@ -82,7 +142,7 @@ fn main() {
 	.title("RUST/libtcod tutorial")
 	.init();
 
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     let mut tcod = Tcod { root, con };
 
@@ -98,24 +158,19 @@ fn main() {
     //the list of objects with those two
     let mut objects = [player, npc];
 
+    let game = Game {
+        map: make_map(),
+    };
+
     while !tcod.root.window_closed() {
 	tcod.con.set_default_foreground(WHITE);
 	for object in &objects {
 	    object.draw(&mut tcod.con);
 	}
 		
-	blit(
-	    &tcod.con,
-	    (0, 0),
-	    (SCREEN_WIDTH, SCREEN_HEIGHT),
-	    &mut tcod.root,
-	    (0, 0),
-	    1.0,
-	    1.0,
-	);
-
         tcod.con.clear();
-	
+
+        render_all(& mut tcod, &game, &objects);
 	tcod.root.flush();
 	tcod.root.wait_for_keypress(true);
 
